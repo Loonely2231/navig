7a871c024679ddb4c5cd7e6a2a198b32dfb57634
Loonely2231 <dmetruk.alexx@gmail.com>
2026-05-27 20:50:38 +0300

    Update script.js
    w

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ navigator.geolocation.getCurrentPosition(function(position) {
     coords = position.coords;
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-    let path = 'https://www.openstreetmap.org/#map=18/' + latitude + '/' + longitude;
-    let link = document.querySelector('.link');
-    link.innerHTML = "<a href='" + path + "'>Посмотреть местоположение</a>";
-    console.log(coords);
+    // let path = 'https://www.openstreetmap.org/#map=18/' + latitude + '/' + longitude;
+    // let link = document.querySelector('.link');
+    // link.innerHTML = "<a href='" + path + "'>Посмотреть местоположение</a>";
+    // console.log(coords);
+    let map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([longitude, latitude]),
+            zoom: 10
+        })
+    })
 });
